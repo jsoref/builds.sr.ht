dcc6fd69e767696ae61228e8af6a39f66226355d
Drew DeVault <sir@cmpwn.com>
2017-04-16 21:50:40 -0400

    Implement user and tag browsing pages

--- buildsrht/blueprints/html.py
+++ buildsrht/blueprints/html.py
@@ -1,34 +1,28 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, abort
 from flask_login import current_user
 from srht.database import db
-from buildsrht.types import Job, JobStatus, TaskStatus
+from buildsrht.types import Job, JobStatus, TaskStatus, User
 from buildsrht.decorators import loginrequired
 from buildsrht.runner import run_build
 
 html = Blueprint("html", __name__)
 
-def tags(job):
-    if not job.tags:
+def tags(tags):
+    if not tags:
         return list()
     previous = list()
     results = list()
-    for tag in job.tags.split("/"):
+    for tag in tags.split("/"):
         results.append({
             "name": tag,
-            "url": "/".join(previous + [tag])
+            "url": "/" + "/".join(previous + [tag])
         })
         previous.append(tag)
-    print(results)
     return results
 
-@html.route("/")
-def index():
-    if not current_user:
-        return render_template("index-logged-out.html")
+def jobs_page(jobs, sidebar, **kwargs):
+    jobs = jobs.order_by(Job.updated.desc())
     page = request.args.get("page")
-    jobs = Job.query\
-        .filter(Job.owner_id == current_user.id)\
-        .order_by(Job.updated.desc())
     total_jobs = jobs.count()
     total_pages = jobs.count() // 10 + 1
     if total_jobs % 10 == 0:
@@ -42,7 +36,7 @@ def index():
     else:
         page = 0
     jobs = jobs.limit(10).all()
-    return render_template("index.html",
+    return render_template("jobs.html",
         jobs=jobs,
         status_map={
             JobStatus.success: "text-success",
@@ -56,5 +50,38 @@ def index():
         sort_tasks=lambda tasks: sorted(tasks, key=lambda t: t.id),
         total_pages=total_pages,
         page=page+1,
-        tags=tags
+        tags=tags,
+        sidebar=sidebar,
+        **kwargs
     )
+
+@html.route("/")
+def index():
+    if not current_user:
+        return render_template("index-logged-out.html")
+    return jobs_page(Job.query.filter(Job.owner_id == current_user.id), "index.html")
+
+@html.route("/jobs/~<username>")
+def user(username):
+    user = User.query.filter(User.username == username).first()
+    if not user:
+        abort(404)
+    jobs = Job.query.filter(Job.owner_id == user.id)
+    if not current_user or current_user.id != user.id:
+        pass # TODO: access controls
+    return jobs_page(jobs, "user.html", user=user, breadcrumbs=[
+        { "name": "~" + user.username, "link": "" }
+    ])
+
+@html.route("/jobs/~<username>/<path:path>")
+def tag(username, path):
+    user = User.query.filter(User.username == username).first()
+    if not user:
+        abort(404)
+    jobs = Job.query.filter(Job.owner_id == user.id)\
+        .filter(Job.tags.ilike(path + "%"))
+    if not current_user or current_user.id != user.id:
+        pass # TODO: access controls
+    return jobs_page(jobs, "user.html", user=user, breadcrumbs=[
+        { "name": "~" + user.username, "url": "" }
+    ] + tags(path))
