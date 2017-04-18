775a9f63986cc6bb769829901457f80b3b5d0f44
Drew DeVault <sir@cmpwn.com>
2017-04-18 09:12:56 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+import subprocess
+import glob
+import os
+
+subprocess.call(["make"])
+
+ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
+      stdout=subprocess.PIPE).stdout.decode().strip()
+
+setup(
+  name = 'buildsrht',
+  packages = [
+      'buildsrht',
+      'buildsrht.types',
+      'buildsrht.blueprints',
+      'buildsrht.alembic',
+      'buildsrht.alembic.versions'
+  ],
+  version = ver,
+  description = 'builds.sr.ht website and runner',
+  author = 'Drew DeVault',
+  author_email = 'sir@cmpwn.com',
+  url = 'https://git.sr.ht/~sircmpwn/builds.sr.ht',
+  install_requires = [
+      'srht',
+      'pgpy',
+      'humanize',
+      'redis',
+      'celery',
+      'yaml',
+      'markdown',
+      'bleach'
+  ],
+  license = 'AGPL-3.0',
+  package_data={
+      'buildsrht': [
+          'templates/*.html',
+          'static/*',
+      ]
+  }
+)
