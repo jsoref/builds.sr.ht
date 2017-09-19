4a599cc3952a5284d3e3275dd9583df28c8a2270
Drew DeVault <sir@cmpwn.com>
2017-09-19 07:52:30 -0400

    Use srht module's meta delegated exchange support

--- buildsrht/oauth.py
+++ buildsrht/oauth.py
@@ -1,13 +1,11 @@
 from srht.config import cfg
 from srht.oauth import OAuthScope, AbstractOAuthService, set_base_service
+from srht.oauth import meta_delegated_exchange
 from srht.flask import DATE_FORMAT
 from srht.database import db
 from buildsrht.types import OAuthToken, User
 from datetime import datetime
-from json import dumps
-import requests
 
-meta = cfg("network", "meta")
 client_id = cfg("meta.sr.ht", "oauth-client-id")
 client_secret = cfg("meta.sr.ht", "oauth-client-secret")
 revocation_url = "{}://{}/oauth/revoke".format(
@@ -25,40 +23,16 @@ class BuildOAuthService(AbstractOAuthService):
         ).first()
         if oauth_token:
             return oauth_token
-        try:
-            r = requests.post("{}/oauth/token/{}".format(meta, token), json={
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "revocation_url": revocation_url
-            })
-            json = r.json()
-        except Exception as ex:
-            print(ex)
-            # TODO: Return 500
-            raise Exception("Temporary authentication failure")
-        if r.status_code != 200:
-            # TODO: ResponseException to include an explicit response?
-            raise Exception("meta.sr.ht error: " + dumps(json))
-        expires = datetime.strptime(json["expires"], DATE_FORMAT)
-        scopes = set(OAuthScope(s) for s in json["scopes"].split(","))
-        try:
-            r = requests.get("{}/api/user/profile".format(meta), headers={
-                "Authorization": "token {}".format(token)
-            })
-            json = r.json()
-        except Exception as ex:
-            # TODO: Return 500
-            print(ex)
-            raise Exception("Temporary authentication failure")
-        if r.status_code != 200:
-            # TODO
-            raise Exception("meta.sr.ht error: " + dumps(json))
-        user = User.query.filter(User.username == json["username"]).first()
+        _token, profile = meta_delegated_exchange(
+                token, client_id, client_secret, revocation_url)
+        expires = datetime.strptime(_token["expires"], DATE_FORMAT)
+        scopes = set(OAuthScope(s) for s in _token["scopes"].split(","))
+        user = User.query.filter(User.username == profile["username"]).first()
         if not user:
             user = User()
-            user.username = json.get("username")
-            user.email = json.get("email")
-            user.paid = json.get("paid")
+            user.username = profile.get("username")
+            user.email = profile.get("email")
+            user.paid = profile.get("paid")
             user.oauth_token = token
             user.oauth_token_expires = expires
             db.session.add(user)
