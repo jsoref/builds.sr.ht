285ece195891d4ae8334d73babceb72f8c661f57
Ludovic Chabant <ludovic@chabant.com>
2019-01-15 02:49:37 +0000

    Use new way of running debug/local services

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from buildsrht.app import app
-from srht.config import cfg, cfgi
-
-import os
-
-app.static_folder = os.path.join(os.getcwd(), "static")
-
 if __name__ == '__main__':
-    app.run(host=cfg("builds.sr.ht", "debug-host"),
-            port=cfgi("builds.sr.ht", "debug-port"),
-            debug=True)
+    from srht.debug import run_service
+    from buildsrht.app import app
+    run_service(app)
