21dbc4c89bfa19d75592b0a142b2c2719f19237b
Denis Laxalde <denis@laxalde.org>
2020-01-23 16:58:16 +0100

    Add a filter to search jobs by status

--- buildsrht/search.py
+++ buildsrht/search.py
@@ -1,9 +1,18 @@
 from srht.search import search
-from buildsrht.types import Job
+from buildsrht.types import Job, JobStatus
 
 def apply_search(query, terms):
     if not terms:
         return query
+
+    def job_status(q, v):
+        try:
+            return q.filter(Job.status == JobStatus(v))
+        except ValueError:
+            return q.filter(False)
+
     default_props = [Job.note]
-    prop_map = {}
+    prop_map = {
+        "status": job_status,
+    }
     return search(query, terms, default_props, prop_map)
