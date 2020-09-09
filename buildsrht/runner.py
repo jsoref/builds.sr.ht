d763ababa703bf8d56c59733ed030f50481c0c24
Drew DeVault <sir@cmpwn.com>
2020-09-09 16:55:51 -0400

    Basic check for ToS issues
    Won't catch the smart ones obviously

--- buildsrht/runner.py
+++ buildsrht/runner.py
@@ -1,6 +1,8 @@
+import json
 from celery import Celery
 from srht.config import cfg
 from srht.database import db
+from srht.email import send_email
 
 runner = Celery('builds', broker=cfg("builds.sr.ht", "redis"), config_source={
     "CELERY_TASK_SERIALIZER": "json",
@@ -14,7 +16,14 @@ def queue_build(job, manifest):
     from buildsrht.types import JobStatus
     job.status = JobStatus.queued
     db.session.commit()
-    run_build.delay(job.id, manifest.to_dict())
+    # crypto mining attempt
+    # pretend to accept it and let the admins know
+    if "xuirig" in json.dumps(manifest.to_dict()):
+        send_email(f"User {job.owner.canonical_name} attempted to submit cryptocurrency mining job #{job.id}",
+                cfg("sr.ht", "owner-email"),
+                "Cryptocurrency mining attempt on builds.sr.ht")
+    else:
+        run_build.delay(job.id, manifest.to_dict())
 
 @runner.task
 def run_build(job_id, manifest):
