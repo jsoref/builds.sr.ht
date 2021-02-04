0585c77f64a4c64613f0e9fe71db76cf6db5c8c9
Francesco Gazzetta <fgaz@fgaz.me>
2021-02-04 22:51:30 +0100

    Add ~/.gitconfig in nixos image

--- images/nixos/image.nix
+++ images/nixos/image.nix
@@ -1,7 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  makeDiskImage = import "${pkgs.path}/nixos/lib/make-disk-image.nix";
+  # Contents attributes are not on stable yet, so we temporarily vendor make-disk-image.
+  # Use this line instead when 21.05 is out:
+  #makeDiskImage = import "${pkgs.path}/nixos/lib/make-disk-image.nix";
+  makeDiskImage = import (pkgs.fetchurl {
+    url = "https://github.com/NixOS/nixpkgs/raw/6f21ae7a626cee022f5c20eacaaf934281e806c0/nixos/lib/make-disk-image.nix";
+    sha256 = "0ggx659i9grw3pzjmpklspp4h3b9dywcjdyh00gjxfvrylyf4qn9";
+  });
   evalConfig = import "${pkgs.path}/nixos/lib/eval-config.nix";
   config = (evalConfig {
     modules = [ (import ./qemu-system-configuration.nix) ];
@@ -12,5 +18,16 @@ in
     lib = pkgs.lib;
     diskSize = 16000;
     format = "qcow2-compressed";
+    contents = [{
+      source = pkgs.writeText "gitconfig" ''
+        [user]
+          name = builds.sr.ht
+          email = builds@sr.ht
+      '';
+      target = "/home/build/.gitconfig";
+      user = "build";
+      group = "users";
+      mode = "644";
+    }];
   }
 
