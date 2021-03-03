fcf9bcb76f1d6c1c546d434a2365533fd09ad3b1
Drew DeVault <sir@cmpwn.com>
2021-03-03 10:53:58 -0500

    API: generalize log fetching
    Will be useful for tasks later

--- api/graph/resolver.go
+++ api/graph/resolver.go
@@ -2,4 +2,49 @@ package graph
 
 //go:generate go run github.com/99designs/gqlgen
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"git.sr.ht/~sircmpwn/builds.sr.ht/api/graph/model"
+)
+
 type Resolver struct{}
+
+func FetchLogs(url string) (*model.Log, error) {
+	// TODO: It might be possible/desirable to set up an API with the runners
+	// we can use to fetch logs in bulk, perhaps gzipped, and set up a loader
+	// for it.
+	client := &http.Client{
+		Timeout: 2 * time.Second,
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Range", "bytes=-131072") // Last 128 KiB
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case http.StatusOK:
+	case http.StatusPartialContent:
+		// OK
+		break
+	default:
+		return nil, fmt.Errorf("Unexpected response from build runner: %s", resp.Status)
+	}
+	limit := io.LimitReader(resp.Body, 131072)
+	log, err := io.ReadAll(limit)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Log {
+		Last128KiB: string(log),
+		FullURL:    url,
+	}, nil
+}
--- api/graph/schema.resolvers.go
+++ api/graph/schema.resolvers.go
@@ -7,9 +7,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"io"
-	"net/http"
-	"time"
 
 	"git.sr.ht/~sircmpwn/builds.sr.ht/api/graph/api"
 	"git.sr.ht/~sircmpwn/builds.sr.ht/api/graph/model"
@@ -41,39 +38,7 @@ func (r *jobResolver) Log(ctx context.Context, obj *model.Job) (*model.Log, erro
 		return nil, nil
 	}
 	url := fmt.Sprintf("http://%s/logs/%d/log", *obj.Runner, obj.ID)
-	// TODO: It might be possible/desirable to set up an API with the runners
-	// we can use to fetch logs in bulk, perhaps gzipped, and set up a loader
-	// for it.
-	client := &http.Client{
-		Timeout: 2 * time.Second,
-	}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Range", "bytes=-131072") // Last 128 KiB
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	switch resp.StatusCode {
-	case http.StatusOK:
-	case http.StatusPartialContent:
-		// OK
-		break
-	default:
-		return nil, fmt.Errorf("Unexpected response from build runner: %s", resp.Status)
-	}
-	limit := io.LimitReader(resp.Body, 131072)
-	log, err := io.ReadAll(limit)
-	if err != nil {
-		return nil, err
-	}
-	return &model.Log {
-		Last128KiB: string(log),
-		FullURL:    url,
-	}, nil
+	return FetchLogs(url)
 }
 
 func (r *jobResolver) Secrets(ctx context.Context, obj *model.Job) ([]model.Secret, error) {
